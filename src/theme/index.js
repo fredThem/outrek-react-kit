7d0501ea817d2bcaf6b22cfd77187dd60222796c
fredThem <fdesmarais.them@gmail.com>
2021-05-31 14:15:24 -0400

    theme init

--- src/theme/index.js
+++ src/theme/index.js
@@ -117,124 +117,124 @@ const themesOptions = {
           input: {
             '&::placeholder': {
               opacity: 0.86,
-              color: '#42526e'
-            }
-          }
-        }
-      }
+              color: '#42526e',
+            },
+          },
+        },
+      },
     },
     palette: {
       action: {
-        active: '#6b778c'
+        active: '#6b778c',
       },
       background: {
         default: '#f4f5f7',
-        paper: '#ffffff'
+        paper: '#ffffff',
       },
       error: {
         contrastText: '#ffffff',
-        main: '#f44336'
+        main: '#f44336',
       },
       mode: 'light',
       primary: {
         contrastText: '#ffffff',
-        main: '#5664d2'
+        main: 'rgb(0, 48, 123)',
       },
       success: {
         contrastText: '#ffffff',
-        main: '#4caf50'
+        main: '#4caf50',
       },
       text: {
         primary: '#172b4d',
-        secondary: '#6b778c'
+        secondary: '#6b778c',
       },
       warning: {
         contrastText: '#ffffff',
-        main: '#ff9800'
-      }
+        main: '#ff9800',
+      },
     },
-    shadows: lightShadows
+    shadows: lightShadows,
   },
   [THEMES.DARK]: {
     components: {
       MuiTableCell: {
         styleOverrides: {
           root: {
-            borderBottom: '1px solid rgba(145, 158, 171, 0.24)'
-          }
-        }
-      }
+            borderBottom: '1px solid rgba(145, 158, 171, 0.24)',
+          },
+        },
+      },
     },
     palette: {
       background: {
         default: '#171c24',
-        paper: '#222b36'
+        paper: '#222b36',
       },
       divider: 'rgba(145, 158, 171, 0.24)',
       error: {
         contrastText: '#ffffff',
-        main: '#f44336'
+        main: '#f44336',
       },
       mode: 'dark',
       primary: {
         contrastText: '#ffffff',
-        main: '#688eff'
+        main: '#688eff',
       },
       success: {
         contrastText: '#ffffff',
-        main: '#4caf50'
+        main: '#4caf50',
       },
       text: {
         primary: '#ffffff',
-        secondary: '#919eab'
+        secondary: '#919eab',
       },
       warning: {
         contrastText: '#ffffff',
-        main: '#ff9800'
-      }
+        main: '#ff9800',
+      },
     },
-    shadows: darkShadows
+    shadows: darkShadows,
   },
   [THEMES.NATURE]: {
     components: {
       MuiTableCell: {
         styleOverrides: {
           root: {
-            borderBottom: '1px solid rgba(145, 158, 171, 0.24)'
-          }
-        }
-      }
+            borderBottom: '1px solid rgba(145, 158, 171, 0.24)',
+          },
+        },
+      },
     },
     palette: {
       background: {
         default: '#1c2531',
-        paper: '#293142'
+        paper: '#293142',
       },
       divider: 'rgba(145, 158, 171, 0.24)',
       error: {
         contrastText: '#ffffff',
-        main: '#f44336'
+        main: '#f44336',
       },
       mode: 'dark',
       primary: {
         contrastText: '#ffffff',
-        main: '#01ab56'
+        main: '#01ab56',
       },
       success: {
         contrastText: '#ffffff',
-        main: '#4caf50'
+        main: '#4caf50',
       },
       text: {
         primary: '#ffffff',
-        secondary: '#919eab'
+        secondary: '#919eab',
       },
       warning: {
         contrastText: '#ffffff',
-        main: '#ff9800'
-      }
+        main: '#ff9800',
+      },
     },
-    shadows: darkShadows
-  }
+    shadows: darkShadows,
+  },
 };
 
 export const createCustomTheme = (config = {}) => {
